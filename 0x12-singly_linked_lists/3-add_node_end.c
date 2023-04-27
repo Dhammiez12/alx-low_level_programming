9f74f3ebcf67991798184a86317679b301b70434
dhammiez12 <adeyinkadamisola@gmail.com>
2023-04-27 08:18:47 -0700

    Linked List in C

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -4,43 +4,73 @@
 #include <stddef.h>
 
 /**
+ * _strlen - A function that gets length of string
+ * @s: string
+ * Return: length of the string
+ */
+
+int _strlen(const char *s)
+{
+	int i;
+
+	for (i = 0; s[i]; i++)
+		;
+	return (i);
+}
+
+/**
+ * _strdup - A function that duplicates a string
+ * @src: source of string
+ * Return: pointer to copied string
+ */
+
+void *_strdup(const char *src)
+{
+	int len, i;
+	char *dest;
+
+	len = _strlen(src);
+	dest = malloc((len + 1) * sizeof(char));
+	if (dest == NULL)
+		return (NULL);
+	for (i = 0; src[i]; i++)
+		dest[i] = src[i];
+	dest[i] = '\0';
+	return (dest);
+}
+
+/**
  * add_node_end - A function that adds a new node
  * at the end of a list_t list
  * @head: First node
  * @str: string
- * Return: pointer to current position
+ * * Return: current position in list
  */
 
 list_t *add_node_end(list_t **head, const char *str)
 {
-	unsigned int size;
-	list_t *new_node;
-	list_t *next_ptr;
-	char *copy_str;
+	list_t *new, *current;
+	char *dupstr;
 
-	new_node = malloc(sizeof(list_t));
-	if (new_node == NULL)
+	if (str == NULL)
 		return (NULL);
-	copy_str = strdup(str);
-	size = 0;
-
-	while (str[size] != '\0')
-		size += 1;
-
-	new_node->str = copy_str;
-	new_node->len = size;
-	new_node->next = NULL;
-
+	dupstr = _strdup(str);
+	if (dupstr == NULL)
+		return (NULL);
+	new = malloc(sizeof(list_t));
+	if (new == NULL)
+		return (NULL);
+	new->str = dupstr;
+	new->len = _strlen(str);
+	new->next = NULL;
 	if (*head == NULL)
 	{
-		*head = new_node;
+		*head = new;
 		return (*head);
 	}
-	next_ptr = *head;
-
-	while (next_ptr->next != NULL)
-		next_ptr = next_ptr->next;
-
-	next_ptr->next = new_node;
+	current = *head;
+	while (current->next != NULL)
+		current = current->next;
+	current->next = new;
 	return (*head);
 }
