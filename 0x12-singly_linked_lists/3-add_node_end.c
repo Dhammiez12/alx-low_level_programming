217020d4856e0f8ca4d21955c16a50be7b2fcc5d
dhammiez12 <adeyinkadamisola@gmail.com>
2023-04-27 08:13:30 -0700

    Linked List in C

--- 0x12-singly_linked_lists/3-add_node_end.c
+++ 0x12-singly_linked_lists/3-add_node_end.c
@@ -4,42 +4,6 @@
 #include <stddef.h>
 
 /**
- * _strlen - A funtion that gets length of string
- * @s: string
- * Return: length of the string
- */
-
-int _strlen(const char *s)
-{
-	int i;
-
-	for (i = 0; s[i]; i++)
-		;
-	return (i);
-}
-
-/**
- * _strdup - A function that duplicates a string
- * @src: source of string
- * Return: pointer to space with copied string
- */
-
-void *_strdup(const char *src)
-{
-	int len, i;
-	char *dest;
-
-	len = _strlen(src);
-	dest = malloc((len + 1) * sizeof(char));
-	if (dest == NULL)
-		return (NULL);
-	for (i = 0; src[i]; i++)
-		dest[i] = src[i];
-	dest[i] = '\0';
-	return (dest);
-}
-
-/**
  * add_node_end - A function that adds a new node
  * at the end of a list_t list
  * @head: First node
