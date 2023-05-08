74dbfbbf6960296ff67e76868d28cd23ed0d2f4e
dhammiez12 <adeyinkadamisola@gmail.com>
2023-05-08 12:34:53 -0700

    File I/O in C

--- 0x15-file_io/0-read_textfile.c
+++ 0x15-file_io/0-read_textfile.c
@@ -10,35 +10,19 @@
 
 ssize_t read_textfile(const char *filename, size_t letters)
 {
-	int f, length, i, res;
-	char *buffer;
+	char *buffer
+	ssize_t fd;
+	ssize_t w;
+	ssize_t t;
 
-	if (filename == NULL)
+	fd = open(filename, O_RDONLY);
+	if (fd == -1)
 		return (0);
-
-
-	f = open(filename, O_RDONLY);
-
-	if (f == -1)
-		return (0);
-
 	buffer = malloc(sizeof(char) * letters);
-	if (!buffer)
-		return (0);
+	t = read(fd, buffer, letters);
+	w = write(STDOUT_FILENO, buffer, t);
 
-	read(f, buffer, letters);
-	buffer[letters] = '\0';
-
-	for (i = 0; buffer[i] != '\0'; i += 1)
-		length += 1;
-
-	res = close(f);
-	if (res != 0)
-		exit(-1);
-	res = write(STDOUT_FILENO, buffer, length);
-	if (res != length)
-		return (0);
 	free(buffer);
-
-	return (length);
+	close(fd);
+	return (w);
 }
--- 0x15-file_io/3-cp.c
+++ 0x15-file_io/3-cp.c
@@ -1,79 +1,99 @@
 #include "main.h"
 #include <stdio.h>
+#include <stdlib.h>
 
+char *create_buffer(char *file);
+void close_file(int fd);
 
 /**
- * error_file - A function that checks if file can be open
- * @file_from: file from
- * @file_to: file to
- * @argv: argument vector
+ * create_buffer - creates  1024 bytes buffer.
+ * @file: file
+ * Return: buffer.
  */
 
-void error_file(int file_from, int file_to, char *argv[])
+char *create_buffer(char *file)
 {
-	if (file_from == -1)
+	char *buffer;
 
-	{
-		dprintf(STDERR_FILENO, "Error: Can't read from file %s\n", argv[1]);
-		exit(98);
-	}
+	buffer = malloc(sizeof(char) * 1024);
 
-	if (file_to == -1)
+	if (buffer == NULL)
 	{
-		dprintf(STDERR_FILENO, "Error: Can't write to %s\n", argv[2]);
+		dprintf(STDERR_FILENO,
+			"Error: Can't write to %s\n", file);
 		exit(99);
 	}
 
+	return (buffer);
 }
 
 /**
- * main - main function
+ * close_file - A function that closes file
+ * @fd: file descriptor
+ */
+
+void close_file(int fd)
+{
+	int cl;
+
+	cl = close(fd);
+
+	if (cl == -1)
+	{
+		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", fd);
+		exit(100);
+	}
+}
+
+/**
+ * main - A function that copies the contents of a file to another file
  * @argc: argument count
  * @argv: argument vector
- * Return: 0
+ * Return: 0 on success
  */
 
 int main(int argc, char *argv[])
 {
-	int file_from, file_to, err_close;
-	ssize_t nrd, nwr;
-	char buffer[1024];
+	int from, to, r, w;
+	char *buffer;
 
 	if (argc != 3)
 	{
-		dprintf(STDERR_FILENO, "%s\n", "Usage: cp file_from file_to");
+		dprintf(STDERR_FILENO, "Usage: cp file_from file_to\n");
 		exit(97);
 	}
-	
-	file_from = open(argv[1], O_RDONLY);
-	file_to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC | O_APPEND, 0664);
-	error_file(file_from, file_to, argv);
-	nrd = 1024;
-	while (nrd == 1024)
-	{
-		
-	nrd = read(file_from, buffer, 1024);
-	if (nrd == -1)
-		error_file(-1, 0, argv);
-	nwr = write(file_to, buffer, nrd);
 
-	if (nwr == -1)
-		error_file(0, -1, argv);
-	}
-	
-	err_close = close(file_from);
-	if (err_close == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-		exit(100);
-	}
-	
-	err_close = close(file_to);
-	if (err_close == -1)
-	{
-		dprintf(STDERR_FILENO, "Error: Can't close fd %d\n", file_from);
-		exit(100);
-	}
+	buffer = create_buffer(argv[2]);
+	from = open(argv[1], O_RDONLY);
+	r = read(from, buffer, 1024);
+	to = open(argv[2], O_CREAT | O_WRONLY | O_TRUNC, 0664);
 
-return (0);
+	do {
+		if (from == -1 || r == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't read from file %s\n", argv[1]);
+			free(buffer);
+			exit(98);
+		}
+
+		w = write(to, buffer, r);
+		if (to == -1 || w == -1)
+		{
+			dprintf(STDERR_FILENO,
+				"Error: Can't write to %s\n", argv[2]);
+			free(buffer);
+			exit(99);
+		}
+
+		r = read(from, buffer, 1024);
+		to = open(argv[2], O_WRONLY | O_APPEND);
+
+	} while (r > 0);
+
+	free(buffer);
+	close_file(from);
+	close_file(to);
+
+	return (0);
 }
